7e2be4b78a76f51c48caacce12e2108e47d74449
stas_stepaniuk <stasstepaniuk@knu.ua>
2022-02-11 19:52:12 +0200

    reaction roles done

--- config.py
+++ config.py
@@ -1,18 +1,11 @@
+import personal_data
 # bot token
-Token = 'OTQxNjQ4NzIyMjg5Mzg5NTc4.YgZAtg.-7Uv8dpG_kLgB_6LpUc4srng-0U'
+Token = personal_data.token
 
 # message users react on
-Message_ID = 941663337593335839
+Message_ID = personal_data.message
 
 # list of roles
-Roles = {
-    'csgo': 941661181163216926,
-    'among_us': 941661222821040159,
-    'meme': 941661277728690226
-}
+Roles = personal_data.roles
 
-RolesToAdd = {
-    941661181163216926: 'Gay',
-    941661222821040159: 'LGBT',
-    941661277728690226: 'Lesbian',
-}
+RolesToAdd = personal_data.rolestoadd
